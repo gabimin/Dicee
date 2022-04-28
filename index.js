20c8f4c319536ee5f2b8fd8a9d3586aed1d8b1ec
Gabi Min <gabimin2030@gmail.com>
2022-04-28 17:06:05 -0300

    icon

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-let randomNumber1 = Math.floor(Math.random() * 6) + 1; 
-console.log(randomNumber1)
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+console.log(randomNumber1);
 
-let randomDiceImage1 = 'dice' + randomNumber1 + '.png'; 
+let randomDiceImage1 = "dice" + randomNumber1 + ".png";
 
-let randomImage1 = '/images/' + randomDiceImage1; 
+let randomImage1 = "/images/" + randomDiceImage1;
 
-let image1=document.querySelectorAll('img')[0];
+let image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute('src', randomImage1);
-
+image1.setAttribute("src", randomImage1);
 
 let randomNumber2 = Math.floor(Math.random() * 6) + 1; //1-6
-console.log(randomNumber2)
+console.log(randomNumber2);
 
-let randomDiceImage2 = 'dice' + randomNumber2 + '.png'; //dice1.png-dice6.png
+let randomDiceImage2 = "dice" + randomNumber2 + ".png"; //dice1.png-dice6.png
 
-let randomImage2 = '/images/' + randomDiceImage2; //images/dice1.png-dice6.png
+let randomImage2 = "/images/" + randomDiceImage2; //images/dice1.png-dice6.png
 
-let image2=document.querySelectorAll('img')[1];
+let image2 = document.querySelectorAll("img")[1];
 
-image2.setAttribute('src', randomImage2);
+image2.setAttribute("src", randomImage2);
 
 if (randomNumber1 > randomNumber2) {
-       document.querySelector('h1').innerHTML = '🏁 Player 1 Wins!';
+  document.querySelector("h1").innerHTML = "🎮 Player 1 Wins!";
 } else if (randomNumber2 > randomNumber1) {
-       document.querySelector('h1').innerHTML = '🏁 Player 2 Wins!';
+  document.querySelector("h1").innerHTML = "🎮 Player 2 Wins!";
 } else {
-       document.querySelector('h1').innerHTML = 'Draw!'
+  document.querySelector("h1").innerHTML = "Draw!";
 }
